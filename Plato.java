975fe0b0b1b2c3ff955d400302947e83dd4afbe1
Simon Ngugi <snmwanjiru@gmail.com>
2024-01-16 23:31:09 -0800

    We are moving first

--- Plato.java
+++ Plato.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 class Plato{
 public static void main(String []args){
 System.out.println("We call it");
+Scanner miko=new Scanner(System.in);
+System.out.println("Enter Your username?");
+String name=miko.next();
+System.out.println("Enter your password?");
+String pass=miko.next();
+System.out.println("\n----------------------------------\n");
+System.out.println("Username:"+name+"\nPassword:"+pass);
+add();
+}
+public static void add()
+{
+    System.out.println("Access Granted");
 }
 }
